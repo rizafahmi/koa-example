034113d4361c3d2fddc6e39b4652d945adec8c55
Riza Fahmi <rizafahmi@gmail.com>
2018-07-02 20:22:14 +0700

    GET and POST

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
+app.use(bodyParser());
+
 router.get('/', async (context) => {
   const { name = 'Koa!' } = context.request.query;
+  context.body = { message: `Hello, ${name}` };
+});
+
+router.post('/', async (context) => {
+  const { name = 'Koa!' } = context.request.body;
   context.body = { message: `Hello, ${name}` };
 });
 
